6eb919f798ff037820e7eaf6c7c065a90f1c5d69
EricStanton <40764581+EricStanton62@users.noreply.github.com>
2018-07-29 17:42:44 -0400

    weekend

--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from .forms import NewTopicForm
 # Create your views here.
 from django.http import HttpResponse, Http404
-from .models import Board
+from .models import Board, Topic, Post
 
 def home(request):
     boards=Board.objects.all()
@@ -11,3 +12,23 @@ def home(request):
 def board_topics(request, pk):
     board=get_object_or_404(Board, pk=pk)
     return render(request,'topics.html', {'board': board})
+
+def new_topic(request, pk):
+    board=get_object_or_404(Board, pk=pk)
+    user = User.objects.first()
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+        )
+            return redirect('board_topics', pk=board.pk)  
+    else:
+        form=NewTopicForm()
+    return render(request, 'new_topic.html', {'board':board, 'form':form})
